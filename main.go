6a80c52dfa4ef26868fd702a5f42c1a06773168f
isurukdniss <isurukdniss@gmail.com>
2024-10-01 13:56:34 +0800

    calculateProduct function is added

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import "fmt"
 func main() {
 	// fmt.Println("Hello World!")
 	fmt.Printf("Sum of %d and %d is %d", 5, 6, calculateSum(5, 6))
+	fmt.Printf("Product of %d and %d is %d", 5, 6, calculateProduct(5, 6))
 }
 
 func calculateSum(x int, y int) int {
 	return x + y
+}
+
+func calculateProduct(x int, y int) int {
+	return x * y
 }
